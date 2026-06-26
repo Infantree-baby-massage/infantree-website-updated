aa331d212da5734ff171194a0504032c769b4b93
Infantree-baby-massage <aslam.shah.work@gmail.com>
2026-06-27 01:19:36 +0530

    Update knowledgeEngine.ts

--- src/engines/knowledgeEngine.ts
+++ src/engines/knowledgeEngine.ts
@@ -1,1 +1,14 @@
+import knowledgeBase from "../data/knowledgeBase.json";
 
+export const getKnowledge = (intent: string) => {
+
+  const item = knowledgeBase.intents.find(
+    (data: any) => data.intent === intent
+  );
+
+  if (!item) {
+    return null;
+  }
+
+  return item;
+};
